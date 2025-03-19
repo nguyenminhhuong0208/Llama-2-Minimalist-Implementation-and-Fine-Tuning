8f9061d973e52275c80cb0b52a1bda0f8bc98a73
nguyenminhhuong0208 <22022542@vnu.edu.vn>
2025-03-20 00:33:32 +0700

    update optimizero.py

--- optimizer.py
+++ optimizer.py
@@ -4,7 +4,6 @@ import torch
 from torch.optim import Optimizer
 import math
 
-
 class AdamW(Optimizer):
     def __init__(
             self,
@@ -39,23 +38,45 @@ class AdamW(Optimizer):
                 if grad.is_sparse:
                     raise RuntimeError("Adam does not support sparse gradients, please consider SparseAdam instead")
 
-                raise NotImplementedError()
+                # raise NotImplementedError()
 
                 # State should be stored in this dictionary
                 state = self.state[p]
 
+                if len(state) == 0:
+                    state['step'] = 0
+                    # Exponential moving average of gradient values
+                    state['exp_avg'] = torch.zeros_like(p.data)
+                    # Exponential moving average of squared gradient values
+                    state['exp_avg_sq'] = torch.zeros_like(p.data)
+
                 # Access hyperparameters from the `group` dictionary
                 alpha = group["lr"]
+                beta1, beta2 = group['betas']
+                weight_decay = group['weight_decay']
 
                 # Update first and second moments of the gradients
+                state['step'] += 1
+                state['exp_avg'] = state['exp_avg'] * beta1 + (1 - beta1) * grad 
+                state['exp_avg_sq'] = state['exp_avg_sq'] * beta2 + (1 - beta2) * grad * grad
 
                 # Bias correction
                 # Please note that we are using the "efficient version" given in
                 # https://arxiv.org/abs/1412.6980
 
+                bias_correction1 = 1 - beta1 ** state['step']
+
+                bias_correction2 = 1 - beta2 ** state['step']
+
                 # Update parameters
 
+                alpha = group['lr'] * math.sqrt((bias_correction2)) / (bias_correction1)
+                denom = (state['exp_avg_sq'] .sqrt() + group['eps'])
+
+                p.data = p.data - alpha * ( state['exp_avg'] / denom)
                 # Add weight decay after the main gradient-based updates.
+                if group['weight_decay'] > 0:
+                    p.data = p.data + (-group['lr']) * p.data * weight_decay
                 # Please note that the learning rate should be incorporated into this update.
 
         return loss
